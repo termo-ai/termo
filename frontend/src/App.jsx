8111644d10f7003adb5c125bc1e31fefa3abf0fa
BrandGerry <91706030+BrandGerry@users.noreply.github.com>
2024-11-22 11:50:46 -0600

    ADD:react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-  return <div className="bg-blue-500 text-white p-4">Hola Tailwind</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Preparado para futuras rutas */}
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Página de Inicio</h1>
+    </div>
+  );
+};
+
+export default Home;

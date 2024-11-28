289bc45803830cc23dcfb9f1e282e25be5fdcd6c
Ailam Cabello <ailamcabello@192.168.2.89>
2024-11-28 16:49:51 -0600

    format-adjust

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,9 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <WebsocketProvider>
-      <App />
-    </WebsocketProvider>
-  </StrictMode>
+    <StrictMode>
+        <WebsocketProvider>
+            <App />
+        </WebsocketProvider>
+    </StrictMode>
 );
